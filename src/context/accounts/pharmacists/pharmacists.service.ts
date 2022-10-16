acc1cb5dfef8a5dec8ac9d8e41b291177244ebc3
magiof <tjddnjs7559@gmail.com>
2022-10-16 20:45:14 +0900

    feat: 약사 회원가입 기능 추가, carts 추가

--- src/context/accounts/pharmacists/pharmacists.service.ts
+++ src/context/accounts/pharmacists/pharmacists.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Pharmacist } from './dto/pharmacist.dto';
 
 @Injectable()
-export class PharmacistsService {}
+export class PharmacistsService {
+  constructor(private prismaService: PrismaService) {}
+
+  async signUpPharmacist(pharmacist: Pharmacist) {
+    const { email, password, nickName, pharmacyName, pharmacyAddress } =
+      pharmacist;
+
+    //TODO: bcrypt 적용해서 비밀번호 암호화 시켜주세요~! 약사는 소금 12번
+    const isExist = await this.prismaService.pharmacist.findFirst({
+      where: { email },
+    });
+    if (isExist) throw new InternalServerErrorException();
+
+    if (!email.includes('@')) throw new BadRequestException();
+
+    const _pharmacist = await this.prismaService.pharmacist.create({
+      data: { email, password, nickName, pharmacyName, pharmacyAddress },
+    });
+
+    return { result: _pharmacist.email, message: '회원가입 완료' };
+  }
+}
