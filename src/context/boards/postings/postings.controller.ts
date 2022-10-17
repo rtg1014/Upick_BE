bddc7e6e38411e268462c181d264ed0e0f51a846
rtg1014 <rtg1014@naver.com>
2022-10-17 23:11:56 +0900

    feat : posting 전체조회, 상세조회 완료

--- src/context/boards/postings/postings.controller.ts
+++ src/context/boards/postings/postings.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { PostingsService } from './postings.service';
 import { Posting } from './dto/postings.dto';
 
@@ -6,9 +6,19 @@ import { Posting } from './dto/postings.dto';
 export class PostingsController {
   constructor(private readonly postingsService: PostingsService) {}
 
-  @Post('create')
+  @Post('')
   createPosting(@Body() posting: Posting) {
     return this.postingsService.createPosting(posting);
+  }
+  
+  @Get(':id')
+  getPosting(@Param('id',ParseIntPipe)id: number){
+    return this.postingsService.getPosting(id)
+  }
+
+  @Get('')
+  getPostings(){
+    return this.postingsService.getPostings()
   }
 
 }
