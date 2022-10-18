0ebdb58f41709d5190a8991973fb7b8d0f939237
rtg1014 <rtg1014@naver.com>
2022-10-19 01:26:54 +0900

    feat : posting 좋아요 기능 추가, createdAt & updateAt 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { JwtMiddleWare } from './middlewares/jwt.middleware';
 import { JwtService } from '@nestjs/jwt';
 import { GoodsModule } from './context/goods/goods.module';
 import { AccountsModule } from './context/accounts/accounts.module';
-import { BoardsModule} from './context/boards/boards.module'
+import { BoardsModule } from './context/boards/boards.module';
 @Module({
   imports: [GoodsModule, AccountsModule, BoardsModule],
   controllers: [AppController],
--- src/context/boards/postings/postings.controller.ts
+++ src/context/boards/postings/postings.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Post, Body, Get, Param, ParseIntPipe, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Put,
+} from '@nestjs/common';
 import { PostingsService } from './postings.service';
 import { Posting } from './dto/postings.dto';
 
@@ -10,19 +19,30 @@ export class PostingsController {
   createPosting(@Body() posting: Posting) {
     return this.postingsService.createPosting(posting);
   }
-  
+
   @Get(':id')
-  getPosting(@Param('id',ParseIntPipe)id: number){
-    return this.postingsService.getPosting(id)
+  getPosting(@Param('id', ParseIntPipe) id: number) {
+    return this.postingsService.getPosting(id);
   }
 
   @Get('')
-  getPostings(){
-    return this.postingsService.getPostings()
+  getPostings() {
+    return this.postingsService.getPostings();
   }
-  
+
   @Patch(':id')
-  updatePosting(@Param('id',ParseIntPipe)id: number,@Body() posting:Posting){
-    return this.postingsService.updatePosting(id,posting)
+  updatePosting(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() posting: Posting,
+  ) {
+    return this.postingsService.updatePosting(id, posting);
+  }
+
+  @Put(':id/like') //FIXME: Body값을 customer로 수정
+  toggleLike(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('customerId') customerId: number,
+  ) {
+    return this.postingsService.toggleLike(id,customerId)
   }
 }
