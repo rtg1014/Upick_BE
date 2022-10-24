09603408582519b6bf5da8870f23a1ecddc10760
mj-song00 <niveus128@gmail.com>
2022-10-24 19:03:00 +0900

    refactor: ci/cd test

--- src/context/accounts/pharmacists/pharmacists.service.ts
+++ src/context/accounts/pharmacists/pharmacists.service.ts
@@ -75,6 +75,7 @@ export class PharmacistsService {
     const secret = process.env.PHARMACIST_JWT_SECRET;
     const expiresIn = '3h';
     const token = sign(payload, secret, { expiresIn });
+    
     return { result: token, message: 'Login success' };
   }
 }
