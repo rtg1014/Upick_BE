ed06d19908743f4924f808b29f0a9af1a5f9d159
mj-song00 <niveus128@gmail.com>
2022-11-11 01:07:29 +0900

    feat: https 추가

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { TransformInterceptor } from './interceptors/transform.interceptor';
 import * as cookieParser from 'cookie-parser';
+import fs from 'fs';
+const httpsOptions = {
+  key: fs.readFileSync('./secrets/private-key.pem'),
+  cert: fs.readFileSync('./secrets/public-certificate.pem'),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
