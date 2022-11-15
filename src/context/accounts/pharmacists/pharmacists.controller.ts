4640f4718e0b4c772e50297b7050243e216628e7
Seong Hyeon Kim <105336416+rtg1014@users.noreply.github.com>
2022-11-16 00:16:33 +0900

    ref: url 에 슬러시 추가

--- src/context/accounts/pharmacists/pharmacists.controller.ts
+++ src/context/accounts/pharmacists/pharmacists.controller.ts
@@ -37,7 +37,7 @@ export class PharmacistsController {
     return this.pharmacistsService.signIn(signInDto);
   }
 
-  @Get('')
+  @Get('/')
   @Roles(ROLE.CUSTOMER)
   getAllPharmacists(){
     return this.pharmacistsService.getAllPharmacists()
