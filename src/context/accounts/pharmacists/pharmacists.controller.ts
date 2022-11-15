06e9bb3dccf899f17e1ff41733ba2b204bb6e5aa
Seong Hyeon Kim <105336416+rtg1014@users.noreply.github.com>
2022-11-15 23:54:56 +0900

    refactor: url 제거

--- src/context/accounts/pharmacists/pharmacists.controller.ts
+++ src/context/accounts/pharmacists/pharmacists.controller.ts
@@ -37,7 +37,7 @@ export class PharmacistsController {
     return this.pharmacistsService.signIn(signInDto);
   }
 
-  @Get('hometown-pharmacist')
+  @Get('')
   @Roles(ROLE.CUSTOMER)
   getAllPharmacists(){
     return this.pharmacistsService.getAllPharmacists()
